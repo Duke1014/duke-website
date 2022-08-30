bfbcb70eb90593ea2da03d28a705f4bc3f00052a
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-08-29 22:00:01 -0400

    contact images functional with links

--- src/Contact.js
+++ src/Contact.js
@@ -8,10 +8,10 @@ import gmail from './img/gmail.png'
 export default function Contact() {
   return (
     <div className='contact'>
-        <div><img src={linkedIn} alt='LinkedIn'/>LinkedIn</div> https://www.linkedin.com/in/brett-duke-norsworthy/
-        <div><img src={github} alt='GitHub'/>GitHub</div> https://github.com/Duke1014/
+        <div><a href='https://www.linkedin.com/in/brett-duke-norsworthy/'><img src={linkedIn} alt='LinkedIn'/></a>LinkedIn</div> 
+        <div><a href='https://github.com/Duke1014/'><img src={github} alt='GitHub'/></a>GitHub</div> 
         {/* <div><img src={twitter} alt='Twitter'/>Twitter</div>  */}
-        <div><img src={gmail} alt="Email"/>Email</div>        
+        <div><a href = "mailto: duke.norsworthy@gmail.com"><img src={gmail} alt="Email"/></a>Email</div>        
     </div>
   )
 }
