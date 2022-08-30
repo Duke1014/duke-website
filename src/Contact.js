62fd04895fabb9b88296a2d95da8dd91b8420960
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-08-29 21:31:49 -0400

    contact images placed

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
+import linkedIn from './img/linkedin.png'
+import github from './img/github.png'
+import gmail from './img/gmail.png'
+// import twitter from './img/twitter.png'
+
 export default function Contact() {
   return (
-    <div>
-        LinkedIn
-        GitHub
-        Twitter
-        Email
+    <div className='contact'>
+        <div><img src={linkedIn} alt='LinkedIn'/>LinkedIn</div> https://www.linkedin.com/in/brett-duke-norsworthy/
+        <div><img src={github} alt='GitHub'/>GitHub</div> https://github.com/Duke1014/
+        {/* <div><img src={twitter} alt='Twitter'/>Twitter</div>  */}
+        <div><img src={gmail} alt="Email"/>Email</div>        
     </div>
   )
 }
