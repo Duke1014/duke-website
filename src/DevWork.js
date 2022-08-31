c4716f6fdb80b7f13d74f6c1abebc111f7bee672
Duke Norsworthy <duke.norsworthy@gmail.com>
2022-08-31 15:53:05 -0400

    image editing and project work

--- src/DevWork.js
+++ src/DevWork.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import ToDoList from './img/ToDoList.png'
+import Soundboard from './img/Soundboard.png'
 
 export default function DevWork() {
 
@@ -8,16 +9,17 @@ export default function DevWork() {
     <div>
         <div className='to-do-list'>
             <h2>To-Do List</h2>
-
-            <img src={ToDoList} alt='To Do List' />
-
-            https://to-do-list-capstone.herokuapp.com/
-
+            <a href='https://to-do-list-capstone.herokuapp.com/'><img src={ToDoList} alt='To Do List' /></a>
             <p>
                 My capstone project for Flatiron School. Since 'apps' have been developed for phones, I've struggled to find a to-do list app that really fit my needs. I plan on further developing this app to get closer to this goal, but what I have done so far makes this a much better product for anyone who uses it.
                 <br/>
-                To add more functionality for others than myself, I've integrated a Group To-do List, where you can create your own group that you can invite others to, and have a joint list to work on. 
+                To add more functionality, I've integrated a Group To-do List, where you can create your own group that you can invite others to, and have a joint list to work on. 
             </p>
+        </div>
+
+        <div className='soundboard'>
+            <h2>Soundboard</h2>
+            <a href='https://rails-project-soundboard.herokuapp.com/'><img src={Soundboard} alt='Soundboard' /></a>
         </div>
     </div>
   )
