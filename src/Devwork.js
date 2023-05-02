e74413cccbb85a42f164d7672a21cce9581e8ce7
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-05-02 02:50:58 -0400

    added more info to cards

--- src/Devwork.js
+++ src/Devwork.js
@@ -22,13 +22,16 @@ export default function Devwork() {
             <p>Doors of Divergence</p>
             {cardFocus === 'dod' ? <>
                 <div className='project-card-info'>
-                    <a href='https://doorsofdivergence.com'>
+                    <a href='https://doorsofdivergence.com' target="_blank" rel="noopener noreferrer">
                         <img src={doorsofdivergence} alt='Doors of Divergence' />
                     </a>             
                     <p>Doors of Divergence is an escape room experience, where you and your friends will work together in a multiverse of stories that are all tied together. They are located in Brooklyn, NY, as a part of the FutureProof building. If you live in the NYC area, I highly recommend any of their escape rooms! Since their story is woven in such an intricate way, <a href='http://doorsofdivergence.com/rift'>I've built their tracking system</a>, where at the end of the experience, you will recieve codes to enter into the site, revealing achievements, lore, and a layout of the story you've seen so far.</p>
                     <br/>
-                    <p>Skills uitilized:</p>
-                    <p>Javascript/React, Google Firebase, Tailwind CSS, Photoshop</p>
+                    <div className='project-card-info-bottom'>
+                        <p>Skills uitilized:</p>
+                        <p>Javascript/React, Google Firebase, Tailwind CSS, Photoshop</p>
+                        <p><a href='https://github.com/Duke1014/DoorsOfDivergence' target='_blank' rel='noopener noreferrer'>Github</a></p>
+                    </div>
                 </div>
             </> : <></>}
         </div>
@@ -39,10 +42,13 @@ export default function Devwork() {
                     <a href='https://shardsofdalaya.com/' target="_blank" rel="noopener noreferrer">
                         <img src={shards} alt='Shards of Dalaya' />
                     </a>
-                    <p>Shards of Dalaya is an emulated EverQuest server, and part of the <a href='http://www.eqemulator.org/'>EQEmu community</a>.</p>
+                    <p>Shards of Dalaya is an emulated EverQuest server, and part of the <a href='http://www.eqemulator.org/'>EQEmu community</a>. Filled with custom content throughout the world, players will find a completely unique experience here compared to the original EverQuest game. I've made a couple of raids, quests, and zones throughout the game, including the powerful Spiny Mole.</p>
                     <br/>
-                    <p>Skills uitilized:</p>
-                    <p>Lua, C++, TortoiseSVN</p>
+                    <div className='project-card-info-bottom'>
+                        <p>Skills uitilized:</p>
+                        <p>Lua, C++, TortoiseSVN</p>
+                        <p><a href='https://shardsofdalaya.com/' target="_blank" rel="noopener noreferrer">Website</a></p>
+                    </div>
                 </div>
             </> : <></>}
         </div>
