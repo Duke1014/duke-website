daa1ede3694be5f2e40af417f880cce0ff63a563
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-06-24 21:16:00 -0400

    mobile css

--- src/DukeText.js
+++ src/DukeText.js
@@ -1,10 +1,37 @@
 export const text = {
-    greetings: {
+    "Start": {
         duketext: "Hello, my name is Duke. What brings you here?",
         responses: [
             "I found your website from LinkedIn | GitHub | your resume!",
             "I just scanned your QR code.",
             "I'm just such a huge fan! 😍"
         ]
+    },
+    "I found your website from LinkedIn | GitHub | your resume!": {
+        duketext: "Amazing! Let me help you with some more info about myself.",
+        responses: [
+            "About Me",
+            "Recent Projects",
+            "Contact Information and Links"
+        ]
+    },
+    "I just scanned your QR code.": {
+        duketext: "Thank you so much for trusting a stranger's QR code.",
+        responses: [
+            "About Me",
+            "Recent Projects",
+            "Contact Information and Links"
+        ]
+    },
+    "I'm just such a huge fan! 😍": {
+        duketext: "Amazing! Let me help you with some more info about myself.",
+        responses: [
+            "About Me",
+            "Recent Projects",
+            "Contact Information and Links"
+        ]
+    },
+    'about': {
+
     }
 }
