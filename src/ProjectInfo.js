25a2cfb2f5fe332c6271d65625a28416cbf5dd91
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-07-10 18:11:05 -0400

    filling in project info for later

--- src/ProjectInfo.js
+++ src/ProjectInfo.js
@@ -2,23 +2,23 @@ export const projectInfo = [
     {
         title: "Dalaya",
         image: "",
-        info: "",
-        skills: [""],
-        links: [""]
+        info: "An Emulated EverQuest server with customized content and variety of gameplay.",
+        skills: ["C++", "Lua", "Game Development and Design", "Internal Web Tool Development (JavaScript, React, PHP, Ruby on Rails)", "SQL", "Data Analytics", "Python"],
+        links: {"Website": "www.dalaya.org"}
     },
     {
-        title: "",
+        title: "Memory Game",
         image: "",
-        info: "",
-        skills: [""],
-        links: [""]
+        info: "Capstone project: a working memory game with working database connection of users and leaderboards with high scores.",
+        skills: ["TypeScript", "React", "PHP", "Game Design", "CSS", "SQL"],
+        links: {"GitHub": ""}
     },
     {
-        title: "",
+        title: "Doors of Divergence",
         image: "",
         info: "",
-        skills: [""],
-        links: [""]
+        skills: ["JavaScript", "React", "Svelte", "Google Firebase", "SQL"],
+        links: {}
     },
     {
         title: "",
