fe9acdec90c4041709712821dcdb140cfba950aa
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-07-12 11:35:28 -0400

    new about text

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ export default function About() {
             </div>
             <section>
                 <div className='about-paragraphs'>
-                    <p>I tried my hand at game development with friends working on an emulated server of EverQuest. Since then, I found myself learning more languages and programming principles, leading me to take on Flatiron School. From there, I learned that developing can be more expansive than creating a game, but even creating the web tools to create and maintain apps is essential and rewarding to build.</p>    
+                    <p>I'm a Full-stack Software engineer with a background in designing and developing games, apps, websites, and internal tools for development.</p>    
                 </div>            
             </section>
         </div>
